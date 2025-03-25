1aaaeb4a24a54e1228890dcc0101c89f19112b63
saint <saint.0x.saint@gmail.com>
2025-03-25 18:46:12 -0400

    zig

--- backend/src/communication/root.zig
+++ backend/src/communication/root.zig
@@ -3,6 +3,18 @@ const core = @import("core");
 
 pub const Communication = @import("main.zig").Communication;
 
-pub fn init(allocator: std.mem.Allocator) !Communication {
-    return Communication.init(allocator);
+pub fn init(allocator: std.mem.Allocator) !*Communication {
+    const comm = try allocator.create(Communication);
+    comm.* = try Communication.init(allocator);
+    return comm;
+}
+
+pub fn deinit(comm: *Communication) void {
+    const allocator = comm.allocator;
+    comm.deinit();
+    allocator.destroy(comm);
+}
+
+pub fn start(comm: *Communication) !void {
+    try comm.start();
 } 
